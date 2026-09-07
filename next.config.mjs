b87049b9a969ea340579dca5a77a91d1da6c00c1
hola243 <hola@littlemucho.com>
2026-09-06 21:50:18 -0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        { source: '/', destination: '/landing.html' },
+      ],
+    };
+  },
+};
 export default nextConfig;
